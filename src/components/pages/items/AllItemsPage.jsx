3a226d09af5bde7e05f0906892efc693c10652bb
Iavor Orlyov <iavor_orlyov@yahoo.com>
2021-10-25 22:24:44 +0300

    Добавяне на погрешно изтрит файл

--- src/components/pages/items/AllItemsPage.jsx
+++ src/components/pages/items/AllItemsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { connect, useSelector } from "react-redux";
-import { FormInput } from "../../common/FormInput";
+import FormInput from "../../common/FormInput";
 import * as itemAction from "../../../redux/actions/itemAction";
 
 const emptyFilter = {
